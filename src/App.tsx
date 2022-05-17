005479aa50422827473dc801ebbe4d1092adaa90
fauzani96 <muhammadfauzanisa@gmail.com>
2022-05-17 22:51:34 +0700

    initial commit template

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {Avatar, Icon, Typography} from '@mui/material'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="p-4 flex flex-col items-center">
+      <div className="flex items-center">
+        <Typography variant="h6">Happy Birthday!</Typography>
+        <Icon className="ml-1">celebration</Icon>
+      </div>
+      <Avatar
+        src="https://asset.kompas.com/crops/EigMGs4Z2XmsY2uJa4b3OFzOkOM=/0x60:930x680/750x500/data/photo/2022/05/04/62723f5727b5f.jpg"
+        alt="people"
+        sx={{width: 56, height: 56}}
+      />
+      <Typography variant="h6">Elizabeth Olsen</Typography>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
